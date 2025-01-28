d08404e40f56487b6f38719ada7427e2399ae098
Aamir Ahmad <aamiropti@gmail.com>
2025-01-28 12:34:08 +0530

    Update takeover.py
    Added new error message for shopify detection

--- takeover.py
+++ takeover.py
@@ -55,6 +55,7 @@ services = {
         "error": r"There isn\\\'t a Github Pages site here\.|a Github Pages site here"
     },
     "Shopify": {"error": r"Sorry\, this shop is currently unavailable\."},
+    "Shopify": {"error": r"Sorry\, this store is currently unavailable\."},
     "Fastly": {"error": r"Fastly error\: unknown domain\:"},
     "Ghost": {
         "error": r"The thing you were looking for is no longer here\, or never was"
